45c609bd31986a3626eda91405c33e370e912a07
정재성 <jaesung@jeongjaeseong-ui-MacBookAir.local>
2022-10-17 14:07:07 +0900

    abc

---
+++ Pythagoras.java
@@ -0,0 +1,11 @@
+public class Pythagoras {
+    public static void main(String[] args) {
+        double a = 3;
+        double b = 4;
+        double res;
+
+        res = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
+
+        System.out.println(res);
+    }
+}
---
+++ error.java
@@ -0,0 +1,7 @@
+public class error {
+    public static void main(String[] args) {
+        System.out.println("스프링 병신");
+        System.err.println("스프링 너무 좋다.");
+
+    }
+}
